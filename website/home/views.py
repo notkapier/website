39a2589069fd20049f91f70e96e4b19913b8129f
notkapier@gmail.com <notkapier@gmail.com>
2019-12-27 00:54:49 -0800

    4th Commit

--- website/home/views.py
+++ website/home/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
+from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
+def index(request):
+	posts = Post.objects.all()
+	return render(request,'home/index.html',{'posts':posts})
