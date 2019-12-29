d668022917fbc1023e7f343af8e1161f9b1ff583
x30n4r4 <notkapier@gmail.com>
2019-12-29 23:01:48 +0700

    completed admin

--- website/home/admin.py
+++ website/home/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post,Announcement,Course,Batch,Library,Reference,Traccer,TraccerItem,Element,PostStatus
 
 admin.site.register(Post)
+admin.site.register(Announcement)
+admin.site.register(Course)
+admin.site.register(Batch)
+admin.site.register(Library)
+admin.site.register(Reference)
+admin.site.register(Traccer)
+admin.site.register(TraccerItem)
+admin.site.register(Element)
+admin.site.register(PostStatus)
--- website/website/urls.py
+++ website/website/urls.py
@@ -28,3 +28,4 @@ urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
+admin.site.site_header = 'CCSICT - Admin Panel'
