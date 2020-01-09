4b4fd89d65de7991bc8472edbd5bcf1e8da7957d
x30n4r4 <notkapier@gmail.com>
2020-01-10 06:25:33 +0700

    fixed static

--- website/website/urls.py
+++ website/website/urls.py
@@ -27,7 +27,9 @@ urlpatterns = [
     
 ]
 urlpatterns += staticfiles_urlpatterns() 
+urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
 admin.site.site_header = 'CCSICT - Admin Panel'
